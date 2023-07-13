456bc2daaf4c5a02aae9b93aa70cc334a26d8dd4
Antonio <achen301@calpoly.edu>
2023-07-13 12:07:57 +0800

    improved formatting

--- StarterClasses/Program.cs
+++ StarterClasses/Program.cs
@@ -1,214 +1,115 @@
-﻿using System;
-using System.Diagnostics.Metrics;
-using System.Net;
-using System.Runtime.InteropServices;
-
-namespace MyApp // Note: actual namespace depends on the project name.
+﻿namespace MyApp // Note: actual namespace depends on the project name.
 {
-  internal class Starters
-  {
-    static void Main(string[] args)
+    internal class Starters
     {
+        static void Main(string[] args)
+        {
+        }
     }
-  }
-  public class Skill
-  {
-    private int cooldownTime;
-    private int cooldownProgress = 0;
-    private bool available = false;
-    private string skillName;
-    private string physicalAttack;
-    public Skill(int cooldownTime, string skillName, string physicalAttack)
+    public class Skill
     {
-      this.cooldownTime = cooldownTime;
-      this.skillName = skillName;
-      this.physicalAttack = physicalAttack;
-    }
-    public void UpdateAvailability()
-    {
-      available = cooldownTime == cooldownProgress;
-    }
-    public void UseSkill()
-    {
-      UpdateAvailability();
-      if (available)
-      {
-        Console.WriteLine($"Using {skillName}");
-        this.cooldownProgress = 0;
-      }
-      else
-      {
-        Console.WriteLine(skillName + " is still charging wait another " + (cooldownTime - cooldownProgress) + "secconds to use this skill");
-      }
-    }
-  }
-
-  interface IStarter
-  {
-    string Speciality { get; }
-    int HP { get; set; }
-    int MP { get; set; }
-    int Level { get; set; }
-    void levelUp();
-    String PhysicalAttack { get; }
-    String ProfessionalName { get; set; }
-    Skill[] Skills { get; }
-  }
-  class beginner : IStarter
-  {
-    public beginner(string PlayerChosenName)
-    {
-      this.ProfessionalName = PlayerChosenName;
+        private int _cooldownTime;
+        private int _cooldownProgress = 0;
+        private bool _available = false;
+        private string _skillName;
+        private string _physicalAttack;
+        public Skill(int cooldownTime, string skillName, string physicalAttack)
+        {
+            _cooldownTime = cooldownTime;
+            _skillName = skillName;
+            _physicalAttack = physicalAttack;
+        }
+        public void UpdateAvailability()
+        {
+            _available = _cooldownTime == _cooldownProgress;
+        }
+        public void UseSkill()
+        {
+            UpdateAvailability();
+            if (_available)
+            {
+                Console.WriteLine($"Using {_skillName}");
+                _cooldownProgress = 0;
+            }
+            else
+            {
+                Console.WriteLine($"{_skillName} is still charging wait another {_cooldownTime - _cooldownProgress} secconds to use this skill");
+            }
+        }
     }
 
-    private string _professionalName = "enter username";
-    public string ProfessionalName
+    interface IStarter
     {
-      get => _professionalName;
-      set=> _professionalName = value;
+        string Speciality { get; }
+        int HP { get; set; }
+        int MP { get; set; }
+        int Level { get; set; }
+        void levelUp();
+        String PhysicalAttack { get; }
+        String ProfessionalName { get; set; }
+        Skill[] Skills { get; }
     }
-    private int _level = 1;
-    public int Level
+    class beginner : IStarter
     {
-      get => _level; 
-      set => _level = value;  
+        public string ProfessionalName { get; set; } = "enter username";
+        public int Level { get; set; } = 1;
+
+        public string Speciality { get; set; } = "beginner";
+        public int HP { get; set; } = 100;
+        public int MP { get; set; } = 50;
+        public string PhysicalAttack { get; private set; } = "punch";
+        public Skill[] Skills { get; private set; } = new Skill[] { new Skill(60, "beginner fire ball", "fire"), new Skill(100, "beginner's lightning", "force lightning") };
+        public beginner(string PlayerChosenName)
+        {
+            ProfessionalName = PlayerChosenName;
+        }
+        public void levelUp()
+        {
+            Level++;
+            HP += 5;
+            MP += 2;
+        }
     }
-    private string _speciality = "beginner";
-    public string Speciality
+    class worrier : IStarter
     {
-      get => _speciality;
+        public string ProfessionalName { get; set; } = "enter username";
+        public int Level { get; set; } = 5;
+
+        public string Speciality { get; set; } = "worrier";
+        public int HP { get; set; } = 140;
+        public int MP { get; set; } = 70;
+        public string PhysicalAttack { get; private set; } = "slash";
+        public Skill[] Skills { get; private set; } = new Skill[] { new Skill(50, "magic shield", ""), new Skill(100, "enraged", "strengthen normal attacks") };
+        public worrier(string PlayerChosenName)
+        {
+            ProfessionalName = PlayerChosenName;
+        }
+        public void levelUp()
+        {
+            Level++;
+            HP += 7;
+            MP += 2;
+        }
     }
-    private int _hP = 100;
-    public int HP
+    class archer : IStarter
     {
-      get => _hP;
-      set => _hP = value;
+        public string ProfessionalName { get; set; } = "enter username";
+        public int Level { get; set; } = 5;
+
+        public string Speciality { get;} = "archer";
+        public int HP { get; set; } = 100;
+        public int MP { get; set; } = 90;
+        public string PhysicalAttack { get; private set; } = "shoot arrow";
+        public Skill[] Skills { get; private set; } = new Skill[] { new Skill(70, "rain arrows", "Area of effect damage"), new Skill(110, "big boy", "an especially strong arrow shot") };
+        public archer(string PlayerChosenName)
+        {
+            ProfessionalName = PlayerChosenName;
+        }
+        public void levelUp()
+        {
+            Level++;
+            HP += 7;
+            MP += 2;
+        }
     }
-    private int _mP = 50;
-    public int MP
-    {
-      get => _mP;
-      set => _mP = value;
-    }
-    private string _physicalAttack = "punch";
-    public string PhysicalAttack
-    {
-      get => _physicalAttack;
-    }
-    public void levelUp()
-    {
-      Level++;
-      HP += 5;
-      MP += 2;
-    }
-    Skill[] _skills = new Skill[] { new Skill(60, "beginner fire ball", "fire"), new Skill(100, "beginner's lightning", "force lightning") };
-    public Skill[] Skills
-    {
-      get => _skills;
-    }
-  }
-  class  worrier: IStarter
-  {
-    public worrier(string PlayerChosenName)
-    {
-      this.ProfessionalName = PlayerChosenName;
-    }
-    private string _professionalName = "enter username";
-    public string ProfessionalName
-    {
-      get => _professionalName;
-      set => _professionalName = value;
-    }
-    private int _level = 5;
-    public int Level
-    {
-      get => _level;
-      set => _level = value;
-    }
-    private string _speciality = "worrier";
-    public string Speciality
-    {
-      get => _speciality;
-    }
-    private int _hP = 140;
-    public int HP
-    {
-      get => _hP;
-      set => _hP = value;
-    }
-    private int _mP = 70;
-    public int MP
-    {
-      get => _mP;
-      set => _mP = value;
-    }
-    private string _physicalAttack = "slash";
-    public string PhysicalAttack
-    {
-      get => _physicalAttack;
-    }
-    public void levelUp()
-    {
-      Level++;
-      HP += 7;
-      MP += 2;
-    }
-    Skill[] _skills = new Skill[] { new Skill(50, "magic shield", ""), new Skill(100, "enraged", "strengthen normal attacks") };
-    public Skill[] Skills
-    {
-      get => _skills;
-    }
-  }
-  class archer : IStarter
-  {
-    public archer(string PlayerChosenName)
-    {
-      this.ProfessionalName = PlayerChosenName;
-    }
-    private string _professionalName = "enter username";
-    public string ProfessionalName
-    {
-      get => _professionalName;
-      set => _professionalName = value;
-    }
-    private int _level = 5;
-    public int Level
-    {
-      get => _level;
-      set => _level = value;
-    }
-    private string _speciality = "archer";
-    public string Speciality
-    {
-      get => _speciality;
-    }
-    private int _hP = 100;
-    public int HP
-    {
-      get => _hP;
-      set => _hP = value;
-    }
-    private int _mP = 90;
-    public int MP
-    {
-      get => _mP;
-      set => _mP = value;
-    }
-    private string _physicalAttack = "shoot arrow";
-    public string PhysicalAttack
-    {
-      get => _physicalAttack;
-    }
-    public void levelUp()
-    {
-      Level++;
-      HP += 7;
-      MP += 2;
-    }
-    Skill[] _skills = new Skill[] { new Skill(70, "rain arrows", "Area of effect damage"), new Skill(110, "big boy", "an especially strong arrow shot") };
-    public Skill[] Skills
-    {
-      get => _skills;
-    }
-  }
 }
