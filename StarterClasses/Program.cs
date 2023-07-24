1b3ca0a0663e0d49f2917fa2128165a6c8696573
Antonio <Antonio0913>
2023-07-24 09:13:08 +0800

    changed ordering and fixed some naming. Also added spacing between classes

--- StarterClasses/Program.cs
+++ StarterClasses/Program.cs
@@ -6,6 +6,7 @@
         {
         }
     }
+
     public class Skill
     {
         private int _cooldownTime;
@@ -13,16 +14,19 @@
         private bool _available = false;
         private string _skillName;
         private string _physicalAttack;
+
         public Skill(int cooldownTime, string skillName, string physicalAttack)
         {
             _cooldownTime = cooldownTime;
             _skillName = skillName;
             _physicalAttack = physicalAttack;
         }
+
         public void UpdateAvailability()
         {
             _available = _cooldownTime == _cooldownProgress;
         }
+
         public void UseSkill()
         {
             UpdateAvailability();
@@ -44,65 +48,74 @@
         int HP { get; set; }
         int MP { get; set; }
         int Level { get; set; }
-        void levelUp();
+        void LevelUp();
         String PhysicalAttack { get; }
         String ProfessionalName { get; set; }
         Skill[] Skills { get; }
     }
-    class beginner : IStarter
+
+    class Beginner : IStarter
     {
         public string ProfessionalName { get; set; } = "enter username";
-        public int Level { get; set; } = 1;
+        public string PhysicalAttack { get; private set; } = "punch";
         public string Speciality { get; set; } = "beginner";
+        public int Level { get; set; } = 1;
         public int HP { get; set; } = 100;
         public int MP { get; set; } = 50;
-        public string PhysicalAttack { get; private set; } = "punch";
         public Skill[] Skills { get; private set; } = new Skill[] { new Skill(60, "beginner fire ball", "fire"), new Skill(100, "beginner's lightning", "force lightning") };
-        public beginner(string PlayerChosenName)
+        
+        public Beginner(string PlayerChosenName)
         {
             ProfessionalName = PlayerChosenName;
         }
-        public void levelUp()
+
+        public void LevelUp()
         {
             Level++;
             HP += 5;
             MP += 2;
         }
     }
-    class worrier : IStarter
+
+    class Worrier : IStarter
     {
         public string ProfessionalName { get; set; } = "enter username";
-        public int Level { get; set; } = 5;
         public string Speciality { get; set; } = "worrier";
+        public string PhysicalAttack { get; private set; } = "slash";
+        public int Level { get; set; } = 5;
         public int HP { get; set; } = 140;
         public int MP { get; set; } = 70;
-        public string PhysicalAttack { get; private set; } = "slash";
         public Skill[] Skills { get; private set; } = new Skill[] { new Skill(50, "magic shield", ""), new Skill(100, "enraged", "strengthen normal attacks") };
-        public worrier(string PlayerChosenName)
+        
+        public Worrier(string PlayerChosenName)
         {
             ProfessionalName = PlayerChosenName;
         }
-        public void levelUp()
+
+        public void LevelUp()
         {
             Level++;
             HP += 7;
             MP += 2;
         }
     }
-    class archer : IStarter
+
+    class Archer : IStarter
     {
         public string ProfessionalName { get; set; } = "enter username";
+        public string Speciality { get; } = "archer";
+        public string PhysicalAttack { get; private set; } = "shoot arrow";
         public int Level { get; set; } = 5;
-        public string Speciality { get;} = "archer";
         public int HP { get; set; } = 100;
         public int MP { get; set; } = 90;
-        public string PhysicalAttack { get; private set; } = "shoot arrow";
         public Skill[] Skills { get; private set; } = new Skill[] { new Skill(70, "rain arrows", "Area of effect damage"), new Skill(110, "big boy", "an especially strong arrow shot") };
-        public archer(string PlayerChosenName)
+        
+        public Archer(string PlayerChosenName)
         {
             ProfessionalName = PlayerChosenName;
         }
-        public void levelUp()
+
+        public void LevelUp()
         {
             Level++;
             HP += 7;
